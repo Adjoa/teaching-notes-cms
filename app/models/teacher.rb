d11f62b406298f6d1c3d1dca9d72a84dd2205594
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-07 22:14:35 +0000

    ensure teacher has_secure_password

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,6 @@
 class Teacher < ActiveRecord::Base
   validates :name, :username, :email, :password, presence: true
+  has_secure_password
   has_many :students
   has_many :entries, through: :students
 end
