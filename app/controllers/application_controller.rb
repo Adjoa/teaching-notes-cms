1243cd35be7a144c37c4572cd6337dad654f9722
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-07 22:15:41 +0000

    app_controller --enable sessions, set session secret, add helpers #logged_in? and #logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,22 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "musicsavedmylife"
   end
 
-  get "/" do
-    erb :welcome
+  get '/' do
+    erb :'home'
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def logout
+      session.clear
+    end
   end
 
 end
