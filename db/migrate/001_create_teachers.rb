0ae03c7099394308b1b13f2156c4b2f663e43ba0
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-07 22:47:06 +0000

    password column should be password_digest for has_secure_password

--- db/migrate/001_create_teachers.rb
+++ db/migrate/001_create_teachers.rb
@@ -4,7 +4,7 @@ class CreateTeachers < ActiveRecord::Migration
       t.string :name
       t.string :username
       t.string :email
-      t.string :password
+      t.string :password_digest
       t.string :info
     end
   end
