b733f6e005a16bb4e92f6f609b113c1a1174e6cf
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-08 20:56:05 +0000

    GET /students/new and POST /students added to students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,32 @@
 class StudentsController < ApplicationController
   get '/students' do
     if logged_in?
+      @teacher = Teacher.find(session[:id])
       erb :'/students/index'
     else
       redirect '/login'
     end
   end
+
+  get '/students/new' do
+    if logged_in?
+      erb :'/students/new_student'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/students' do
+    # binding.pry
+    # @student = Student.new(params[:student])
+    @teacher = Teacher.find(session[:id])
+    @student = @teacher.students.build(params[:student])
+    if @student.valid?
+      @student.save
+      redirect '/students'
+    else
+      redirect '/students/new'
+    end
+  end
+
 end
