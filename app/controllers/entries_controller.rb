b3e253a9a561c346694206c5745ed6f45b7799a5
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-09 15:08:24 +0000

    entries controller --added POST /entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,4 +7,16 @@ class EntriesController < ApplicationController
       redirect '/login'
     end
   end
+
+  post '/entries' do
+    @student = Student.find(session[:student_id])
+    @entry = @student.entries.build(params[:entry])
+    if @entry.valid?
+      @entry.save
+      redirect "/students/#{@student.id}"
+    else
+      redirect '/entries/new'
+    end
+  end
+
 end
