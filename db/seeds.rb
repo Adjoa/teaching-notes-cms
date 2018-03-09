e77655f64626ccc3f49b63331cae3a69e61e29ce
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-09 13:51:18 +0000

    teacher -> teacher1 to match original instantiation in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ seed_students = [
 ]
 
 seed_students.each do |seedling|
-  student = teacher.students.create(seedling)
+  student = teacher1.students.create(seedling)
   entry = student.entries.create(title: "Entry 1", content: "Something nice")
   entry.save
 end
