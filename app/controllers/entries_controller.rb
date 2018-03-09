980b5bc01b67d525efc97232bc4bde890adedfc0
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-09 13:48:13 +0000

    pass student_id to new entry view via EntriesController

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,7 @@
 class EntriesController < ApplicationController
   get '/entries/new' do
     if logged_in?
+      @student = Student.find(session[:student_id])
       erb :'entries/new_entry'
     else
       redirect '/login'
