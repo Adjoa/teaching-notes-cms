9f96c3631caeeceecf3de7dd5a483b682a7b0a2a
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-14 18:15:29 +0000

    removed password presence validation, has_secure_password does this automatically

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,5 @@
 class Teacher < ActiveRecord::Base
-  validates :name, :username, :email, :password, presence: true
+  validates :name, :username, :email, presence: true
   validates :username, :email, uniqueness: true
   has_secure_password
   has_many :students
